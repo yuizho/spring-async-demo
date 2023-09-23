9fcf4b3fb0ead6a8329662c35e1fac816e55bd22
yuizho <yuizho3@gmail.com>
2023-09-23 14:26:43 +0900

    modify ThreadPoolTaskExecutor options

--- src/main/java/dev/yuizho/asyncdemo/asyncdemo/AsyncDemoApplication.java
+++ src/main/java/dev/yuizho/asyncdemo/asyncdemo/AsyncDemoApplication.java
@@ -20,9 +20,24 @@ public class AsyncDemoApplication {
     public Executor taskExecutor() {
         // https://spring.pleiades.io/spring-framework/docs/current/javadoc-api/org/springframework/scheduling/concurrent/ThreadPoolTaskExecutor.html#getQueueCapacity()
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(500);
+        // CorePoolサイズは常にアイドル状態のタスク
+        // taskの量がQueueCapacityを超えるとMaxPoolサイズまでThreadが増える
+        // ただし、MaxPoolサイズまで増えた状態で、更にQueueCapacityを超えるとTaskRejectedExceptionの例外が発生してしまう
+        //
+        // 今回の例だと、タスクの総数: 20 なので、
+        // 10タスクが10スレッドによってさばかれて、残りの10タスクはキューイングされる
+        // 最初の10タスクが完了したら、残りの10タスクがさばかれることになる。
+        // これを踏まえて、タスクの総数: 21 だった場合、
+        // 10タスクが10スレッドによってさばかれて、残りの11タスクはキューイングされることになる。
+        // このときキューイングされるタスクの量が10(QueueCapacity)を超えるのでTaskRejectedExceptionの例外が発生してしまう
+        // https://blog.ik.am/entries/443
+        //
+        // QueueCapacityを超えた分作られるThreadの数はQueueCapacityの値とタスクの数に依存する
+        // 例えば、アイドルスレッド1で、固定の5タスクを5スレッドさばきたかったらMaxPoolSize: 5, QueueCapacity: 0にすればよい
+        // このとき、QueueCapacity: 1にすると4スレッドでさばかれ、2にするとスレッドでさばかれる感じになる
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(10);
         executor.setThreadNamePrefix("TaskExecutor-");
         executor.initialize();
         return executor;
